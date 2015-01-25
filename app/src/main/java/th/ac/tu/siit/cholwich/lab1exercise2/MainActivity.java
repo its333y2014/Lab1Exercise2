7701932fcac27f995e20f638719afcf10c96cf77
Cholwich Nattee <cholwich@gmail.com>
2015-01-25 15:50:08 +0700

    Update MainActivity.java
    Finish the solution

--- app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
+++ app/src/main/java/th/ac/tu/siit/cholwich/lab1exercise2/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +16,61 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void convert(View v) {
+        //Get the unit to convert from
+        RadioGroup rgFrom = (RadioGroup)findViewById(R.id.rgFrom);
+        int idFrom = rgFrom.getCheckedRadioButtonId();
+
+        //Get the unit to convert to
+        RadioGroup rgTo = (RadioGroup)findViewById(R.id.rgTo);
+        int idTo = rgTo.getCheckedRadioButtonId();
+
+        //Get the input value
+        EditText etInput = (EditText)findViewById(R.id.etInput);
+        double input = Double.parseDouble(etInput.getText().toString());
+
+        //Initial the output value to the input value
+        double output = input;
+
+        //Convert the temperature value
+        switch (idFrom) {
+            case R.id.rbFrmC:
+                switch (idTo) {
+                    case R.id.rbToF:
+                        output = input * 9.0 / 5.0 + 32.0;
+                        break;
+                    case R.id.rbToK:
+                        output = input + 273.15;
+                        break;
+                }
+                break;
+            case R.id.rbFrmF:
+                switch (idTo) {
+                    case R.id.rbToC:
+                        output = (input - 32.0) * 5.0 / 9.0;
+                        break;
+                    case R.id.rbToK:
+                        output = (input + 459.67) * 5.0 / 9.0;
+                        break;
+                }
+                break;
+            case R.id.rbFrmK:
+                switch (idTo) {
+                    case R.id.rbToC:
+                        output = input - 273.15;
+                        break;
+                    case R.id.rbToF:
+                        output = input * 9.0 / 5.0 - 459.67;
+                        break;
+                }
+                break;
+        }
+
+        //Display the output
+        TextView tv = (TextView)findViewById(R.id.tvOutput);
+        tv.setText(String.format("%.2f", output));
     }
 
 
